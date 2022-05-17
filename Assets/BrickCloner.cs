66e7bc4f03885d90b853f513fec684cac487509d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-05-17 16:17:06 -0300

    Initial commit

---
+++ Assets/BrickCloner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickCloner : MonoBehaviour
+{
+    public GameObject brickPrefab;
+    public float xOffset;
+    public float userOffsetFloat;
+    public Text userOffset;
+    int counter = 0;
+
+    public void CloneBrick()
+    {
+        if(counter == 0){
+            userOffsetFloat = float.Parse(userOffset.text, System.Globalization.CultureInfo.InvariantCulture);
+            xOffset = userOffsetFloat;
+        }
+
+        if (counter < 5)
+        {
+            GameObject clon;
+            clon = Instantiate(brickPrefab);
+            clon.transform.position += new Vector3(xOffset, 0, 0);
+            xOffset += userOffsetFloat;
+            counter++;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Repaso-2-bimestre-4to-E
+ 
